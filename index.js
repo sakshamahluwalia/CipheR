ed1efb02d5db34a770585873795bb1d93413a6d4
ahluwa41 <saksham.ahluwalia@mail.utoronto.ca>
2019-01-29 20:01:37 -0500

    A user can hide what he is typing now starting decoding

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 // variables for creating a string.
-var blob = " ";
-var word = " ";
+var blob = "";
+var word = "";
 
 var input = document.querySelector("#input");
 
 // Listen to any, "keypress" event.
 addEventListener('keypress', function(event) {
 
-	word += event.key + 1;
+	// console.log(event);
 
+	// console.log(event.key + " " + event.key+1)
 
 	// if the key pressed is a, "space character" start a new word.
 	if (event.code == "Space") {
-		blob = word + " ";
+		word += " ";
+		blob += word;
+		reset();
+	} else {
+		word += String.fromCharCode(event.charCode+1);
 	}
 
-	// input.text = word;
-	console.log(blob);
+	if (event.keyCode == 8) { console.log("back"); }
+
+	input.innerText = blob;
+	// console.log(blob);
 
 });
 
